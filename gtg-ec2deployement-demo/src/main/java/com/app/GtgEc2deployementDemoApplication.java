ccda6823ace220c9e49fa4bfc9b6ac3ca93ecb5a
vishnuawasthi <vishnuawasthi121@gmail.com>
2020-03-14 22:23:16 +0530

    Initial commit

---
+++ gtg-ec2deployement-demo/src/main/java/com/app/GtgEc2deployementDemoApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtgEc2deployementDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtgEc2deployementDemoApplication.class, args);
+	}
+
+}
---
+++ gtg-ec2deployement-demo/src/test/java/com/app/GtgEc2deployementDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtgEc2deployementDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
